1910a2eec546c5509ba041036cc7135957c35028
Alexis Perez <alexisperez00.cappi@gmail.com>
2021-10-20 16:21:24 -0500

    Ejercicio-1 completado

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 //Imports de los módulos
 //fs y path
+const fs = require('fs').promises
+const path = require('path')
+const pathUser = path.resolve("users.json")
 
 const getUsers = async() => {
     //Leer el archivo
-
+    try {
+        const users = await fs.readFile(pathUser, {encoding: "utf8"})
     //Regresar el arreglo de usuarios como objeto literal Javascript (sin notación JSON)
+        return JSON.parse(users)
+    } catch (error) {
+            console.log(error)
+    }
 };
 
 const addUser = async (userObj) => {
-    //leer el archivo 
-
-    //convertir el contenido en formato JSON en un objeto JS
-
-    //agregar el usuario en el arreglo
-
-    //sobreescribir el arreglo en el archivo
+    try {
+        //leer el archivo
+        const users = await getUsers()
+        //convertir el contenido en formato JSON en un objeto JS
+        //agregar el usuario en el arreglo
+        users.push(userObj)
+        //sobreescribir el arreglo en el archivo
+        fs.writeFile(pathUser, JSON.stringify(users))
 
     //si el proceso se realizó correctamente tendrás que regresar el objeto de usuario
     //que acabas de agregar en el arreglo
+        return userObj
+    } catch (error) {
+        console.log(error)        
+    } 
+
+    
 };
 
 const clearUsers = async () => {
-    //Vaciar el arreglo y sobreescribir el archivo
+    try {
+        //Vaciar el arreglo y sobreescribir el archivo
+        await fs.writeFile(pathUser, JSON.stringify([]))
+        //Si el proceso se realizó correctamente tendrás que regresar true
+        return true
+    } catch (error) {
+        console.log(error)        
+    }
     
-    //Si el proceso se realizó correctamente tendrás que regresar true
 }
 
 module.exports = {
